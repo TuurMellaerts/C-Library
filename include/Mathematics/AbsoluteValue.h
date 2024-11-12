26f6b2828edd28f1f29c961339689803096e13ae
tuurmellaerts <tuur.mellaerts1@hotmail.be>
2024-11-12 19:35:57 +0100

    Add functions to calculate absolute value of int16_t, int32_t and int64_t.

--- include/Mathematics/AbsoluteValue.h
+++ include/Mathematics/AbsoluteValue.h
@@ -14,3 +14,24 @@
  * @return `int8_t` the absolute value of the given number
  */
 int8_t AbsoluteValue_SignedInteger8(int8_t number);
+
+/**
+ * @brief Returns the absolute value of the given `int16_t` number.
+ * @param number the given number
+ * @return `int16_t` the absolute value of the given number
+ */
+int16_t AbsoluteValue_SignedInteger16(int16_t number);
+
+/**
+ * @brief Returns the absolute value of the given `int32_t` number.
+ * @param number the given number
+ * @return `int32_t` the absolute value of the given number
+ */
+int32_t AbsoluteValue_SignedInteger32(int32_t number);
+
+/**
+ * @brief Returns the absolute value of the given `int64_t` number.
+ * @param number the given number
+ * @return `int64_t` the absolute value of the given number
+ */
+int64_t AbsoluteValue_SignedInteger64(int64_t number);
--- src/Mathematics/AbsoluteValue.c
+++ src/Mathematics/AbsoluteValue.c
@@ -11,3 +11,18 @@ int8_t AbsoluteValue_SignedInteger8(int8_t number)
 {
     return (number < 0) ? -number : number;
 }
+
+int16_t AbsoluteValue_SignedInteger16(int16_t number)
+{
+    return (number < 0) ? -number : number;
+}
+
+int32_t AbsoluteValue_SignedInteger32(int32_t number)
+{
+    return (number < 0) ? -number : number;
+}
+
+int64_t AbsoluteValue_SignedInteger64(int64_t number)
+{
+    return (number < 0) ? -number : number;
+}
